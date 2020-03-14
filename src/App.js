c09de427bf501dd19e8b480fc4855f56e871d805
Aamir Iqubal <37740469+AamirSoftwareDeveloper@users.noreply.github.com>
2020-03-14 18:08:05 +0200

    Added about the App, heading and title

--- src/App.js
+++ src/App.js
@@ -155,6 +155,14 @@ const App = () => {
   const prevGridValues = getPrevGridValues();
   return (
     <div className="change-numbers">
+      <h1>Fibonacci Player</h1>
+      <p>
+        When you click on a cell, all values in the cells in the same row and
+        column are increased by 1 or, if a cell was empty, it will get a value
+        of 1. After each change a cell will briefly turn yellow. If 5
+        consecutive numbers in the Fibonacci sequence are next to each other,
+        these cells will briefly turn green and will be cleared.
+      </p>
       {objectGrid.map((value, rowIndex) => (
         <React.Fragment key={rowIndex}>
           <div className="row" key={`row-${rowIndex}`}>
